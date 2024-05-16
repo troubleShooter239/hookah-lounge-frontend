2e71323d78497e54817a070b856785b6c8a6fa3d
troubleShooter239 <morgun2282@gmail.com>
2024-05-16 14:20:10 +0300

    fix code readability

--- js/hookahs.js
+++ js/hookahs.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('.element').each(function() {
-        $(this).mouseover(function() {
-            $(this).addClass('active');
-            $('.element').not(this).addClass('inactive');
+$(document).ready(function () {
+    $(".element").each(function () {
+        $(this).mouseover(function () {
+            $(this).addClass("active");
+            $(".element").not(this).addClass("inactive");
         });
-        $(this).mouseleave(function() {
-            $(this).removeClass('active');
-            $('.element').not(this).removeClass('inactive');
+        $(this).mouseleave(function () {
+            $(this).removeClass("active");
+            $(".element").not(this).removeClass("inactive");
         });
     });
 });
--- js/includeHtml.js
+++ js/includeHtml.js
@@ -2,7 +2,7 @@ function includeHTML(file, container) {
     $(container).load(file);
 }
 
-includeHTML('/layout/header.html', '#header');
-includeHTML('/layout/footer.html', '#footer')
-includeHTML('/layout/besties.html', '#besties')
-includeHTML('/layout/story.html', '#story')
+includeHTML("/layout/header.html", "#header");
+includeHTML("/layout/footer.html", "#footer");
+includeHTML("/layout/besties.html", "#besties");
+includeHTML("/layout/story.html", "#story");
